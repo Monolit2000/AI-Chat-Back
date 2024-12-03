ac979ead489b41866ebe4343e6903bbf7482f15c
Monolit2000 <46726840+Monolit2000@users.noreply.github.com>
2024-12-03 14:30:49 +0200

    commit

--- AudioProcessing.Aplication/MediatR/Chats/GeneareteChatTitel/GeneareteChatTitelCommandHandler.cs
+++ AudioProcessing.Aplication/MediatR/Chats/GeneareteChatTitel/GeneareteChatTitelCommandHandler.cs
@@ -1,13 +1,7 @@
 ﻿using AudioProcessing.Aplication.Common.Contract;
-using AudioProcessing.Aplication.Common.Models;
 using AudioProcessing.Domain.Chats;
 using FluentResults;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AudioProcessing.Aplication.MediatR.Chats.GeneareteChatTitel
 {
@@ -18,8 +12,10 @@ namespace AudioProcessing.Aplication.MediatR.Chats.GeneareteChatTitel
         public async Task<Result<ChatTitelDto>> Handle(GeneareteChatTitelCommand request, CancellationToken cancellationToken)
         {
 
-            var newChatTitel = await ollamaService.GenerateResponce(
-                new OllamaRequest($"Come up with one name for this chat in accordance with this prompt: {request.Prompt} You must give only one name, that is, your answer must consist of only one word!"));
+            //var newChatTitel = await ollamaService.GenerateResponce(
+            //    new OllamaRequest($"Come up with one name for this chat in accordance with this prompt: {request.Prompt} You must give only one name, that is, your answer must consist of only one word!"));
+
+            var newChatTitel = "testname";
 
             var chat = await chatRepository.GetByIdAsync(new ChatId(request.ChatId));
 
